8ba3fff1e068aa9ac6fba0b5ffbcaad232ea3550
Yasmeen bano <yasmeenbano1906@gmail.com>
2026-08-17 23:52:08 +0530

    Added Contact Page

---
+++ src/contact.js
@@ -0,0 +1,12 @@
+// CSS
+import "./css/global.css";
+import "./css/navbar.css";
+import "./css/footer.css";
+import "./css/sections.css";
+import "./css/contact.css";
+
+// JavaScript
+import "./js/icons.js"
+import "./js/navbar.js";
+import "./js/scrollAnimations.js";
+
