cce4903354875191b5f403d500e83a82ac12cf1c
Yasmeen bano <yasmeenbano1906@gmail.com>
2026-08-28 17:21:54 +0530

    Add About page

--- src/about.js
+++ src/about.js
@@ -1,11 +1,11 @@
-// CSS 
+// CSS
 import "./css/global.css";
 import "./css/navbar.css";
-import "./css/sections.css";
 import "./css/footer.css";
-import "./css/resources.css";
+import "./css/sections.css";
+import "./css/about.css";
 
-// JAVASCRIPT 
-import "./js/icons.js";
+// JavaScript
+import "./js/icons.js"
 import "./js/navbar.js";
 import "./js/scrollAnimations.js";
